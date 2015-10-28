a9a80ee106a27f2a9af3bd28bb458615a7cc3056
Sebor <al.sprut@gmail.com>
2015-10-28 16:15:34 +0300

    Rework check_func

--- builder.py
+++ builder.py
@@ -22,73 +22,98 @@ def create_db(source_dir):
 			md5 = hashlib.md5(file).hexdigest()
 			Datetime = datetime.datetime.now()
 			cur.execute("INSERT INTO PACKAGES VALUES (?,?,?,?,?);", (file, State, md5, Datetime, Depends))
-        return sys.exit()
+	return sys.exit()
 
 
 def check_func(source_dir):
-    if os.path.isfile("packages.db"):
-        con = lite.connect('packages.db')
-        with con:
-            cur = con.cursor()
-            cur.execute("SELECT Name from PACKAGES WHERE State = 'Not Built'")
-            print cur.fetchone()
-    else:
-        print "DB file doesn't exist. Creating DB..."
-        create_db(source_dir)
-    return sys.exit()
+	if os.path.isfile("packages.db"):
+		con = lite.connect('packages.db')
+		with con:
+			cur = con.cursor()
+			cur.execute("CREATE TABLE NEW_PACKAGES(Name TEXT, MD5 TEXT)")
+			for file in os.listdir(source_dir):
+				md5 = hashlib.md5(file).hexdigest()
+				cur.execute("INSERT INTO NEW_PACKAGES VALUES (?,?);", (file, md5))
+			cur.execute("SELECT Name, MD5 FROM PACKAGES")
+			old_data = dict(cur.fetchall())
+			cur.execute("SELECT Name, MD5 FROM NEW_PACKAGES")
+			new_data = dict(cur.fetchall())
+			old_data_set = set(old_data.iteritems())
+			new_data_set = set(new_data.iteritems())
+			if len(new_data_set.difference(old_data_set)) != 0:
+				State = 'unknown'
+				Depends = 'unknown'
+				for pkg in new_data_set.difference(old_data_set):
+					if pkg[0] not in old_data.keys():
+						Name = pkg[0]
+						md5 = pkg[1]
+						Datetime = datetime.datetime.now()
+						cur.execute("INSERT INTO PACKAGES VALUES (?,?,?,?,?);", (Name, State, md5, Datetime, Depends))
+					else:
+						Name = pkg[0]
+						md5 = pkg[1]
+						cur.execute("UPDATE PACKAGES SET MD5 = ?, State = ?, Depends = ? WHERE Name = ?", [md5, Name, State, Depends])
+			con.commit()
+			cur.execute("DROP TABLE NEW_PACKAGES")
+			cur.execute("SELECT Name from PACKAGES WHERE State = 'Not Built' OR State = 'unknown'")
+			print cur.fetchall()
+	else:
+		print "DB file doesn't exist. We don't have information about packages. Creating DB..."
+		create_db(source_dir)
+	return sys.exit()
 
 
 def build_func(source_dir, dest_dir):
-    if not os.path.isfile("packages.db"):
-        force_rebuild_func(source_dir, dest_dir)
-    else:
-        con = lite.connect('packages.db')
-        with con:
-            State = ''
-            md5 = ''
-            Datetime = ''
-            Depends = ''
-            cur = con.cursor()
-            cur.execute("CREATE TABLE NEW_PACKAGES(Name TEXT, State INT, MD5 TEXT, DATETIME TEXT, Depends TEXT)")
-            for file in os.listdir(source_dir):
-                md5 = hashlib.md5(file).hexdigest()
-                Datetime = datetime.datetime.now()
-                cur.execute("INSERT INTO NEW_PACKAGES VALUES (?,?,?,?,?);", (file, State, md5, Datetime, Depends))
-            cur.execute("SELECT DISTINCT Name FROM NEW_PACKAGES  WHERE Name Not IN (SELECT DISTINCT Name FROM PACKAGES)")
-            new_pkg = cur.fetchone()
-        for newpkg in new_pkg:
-            args = ['rpmbuild', '--define', '_topdir ', dest_dir, '--rebuild', source_dir + os.path.sep + newpkg]
-            ExitCode = subprocess.check_call(args)
-            md5 = hashlib.md5(newpkg).hexdigest()
-            Datetime = datetime.datetime.now()
-            if ExitCode == 0:
-                State = 'Built'
-                with con:
-                    cur = con.cursor()
-                    cur.execute("INSERT INTO PACKAGES VALUES (?,?,?,?,?);", (newpkg, State, md5, Datetime, Depends))
-                    cur.execute("DROP TABLE NEW_PACKAGES")
-            else:
-                State = 'Not Built'
-                with con:
-                    cur = con.cursor()
-                    cur.execute("INSERT INTO PACKAGES VALUES (?,?,?,?,?);", (newpkg, State, md5, Datetime, Depends))
-                    cur.execute("DROP TABLE NEW_PACKAGES")
+	if not os.path.isfile("packages.db"):
+		force_rebuild_func(source_dir, dest_dir)
+	else:
+		con = lite.connect('packages.db')
+		with con:
+			State = ''
+			md5 = ''
+			Datetime = ''
+			Depends = ''
+			cur = con.cursor()
+			cur.execute("CREATE TABLE NEW_PACKAGES(Name TEXT, State INT, MD5 TEXT, DATETIME TEXT, Depends TEXT)")
+			for file in os.listdir(source_dir):
+				md5 = hashlib.md5(file).hexdigest()
+				Datetime = datetime.datetime.now()
+				cur.execute("INSERT INTO NEW_PACKAGES VALUES (?,?,?,?,?);", (file, State, md5, Datetime, Depends))
+			cur.execute("SELECT DISTINCT Name FROM NEW_PACKAGES  WHERE Name Not IN (SELECT DISTINCT Name FROM PACKAGES)")
+			new_pkg = cur.fetchall()
+		for newpkg in new_pkg:
+			args = ['rpmbuild', '--define', '_topdir ', dest_dir, '--rebuild', source_dir + os.path.sep + newpkg]
+			ExitCode = subprocess.check_call(args)
+			md5 = hashlib.md5(newpkg).hexdigest()
+			Datetime = datetime.datetime.now()
+			if ExitCode == 0:
+				State = 'Built'
+				with con:
+					cur = con.cursor()
+					cur.execute("INSERT INTO PACKAGES VALUES (?,?,?,?,?);", (newpkg, State, md5, Datetime, Depends))
+					cur.execute("DROP TABLE NEW_PACKAGES")
+			else:
+				State = 'Not Built'
+				with con:
+					cur = con.cursor()
+					cur.execute("INSERT INTO PACKAGES VALUES (?,?,?,?,?);", (newpkg, State, md5, Datetime, Depends))
+					cur.execute("DROP TABLE NEW_PACKAGES")
 
 
 def force_rebuild_func(source_dir, dest_dir):
-    if os.path.isfile("packages.db"):
-        os.remove("packages.db")
-        create_db(source_dir)
-    else:
-        create_db(source_dir)
-    for srcrpm in os.listdir(source_dir):
-        args = ['rpmbuild', '--define', '_topdir ', dest_dir, '--rebuild', source_dir + os.path.sep + srcrpm]
-        subprocess.call(args)
+	if os.path.isfile("packages.db"):
+		os.remove("packages.db")
+		create_db(source_dir)
+	else:
+		create_db(source_dir)
+	for srcrpm in os.listdir(source_dir):
+		args = ['rpmbuild', '--define', '_topdir ', dest_dir, '--rebuild', source_dir + os.path.sep + srcrpm]
+		subprocess.call(args)
 
 
 def check_deps_func(source_dir):
-    con = lite.connect('packages.db')
-    for srcrpm in os.listdir(source_dir):
+	con = lite.connect('packages.db')
+	for srcrpm in os.listdir(source_dir):
 		args = ['sudo', 'yum-builddep', '-y', source_dir + os.path.sep + srcrpm]
 		ExitCode = subprocess.call(args)
 		if ExitCode == 0:
